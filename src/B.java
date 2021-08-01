73ee3cb50d205e63a7d74c95ab5b5174e2c447b0
malodetz <malo.detz@yandex.ru>
2021-08-01 19:14:19 +0300

    First method transferred

--- src/B.java
+++ src/B.java
@@ -2,6 +2,12 @@ import A;
 
 public class B {
 
+    void FirstMethod(int x) {
+        for (int i = 0; i < x; i++) {
+            SecondMethod(x, i);
+        }
+    }
+
     int ThirdMethod(int a) {
         A x = new A;
         return SecondMethod(a, 2 * a);
