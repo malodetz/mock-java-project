d7e7fd7207962949e4af81205c1af2cd0e3a7557
malodetz <malo.detz@yandex.ru>
2021-08-01 19:12:58 +0300

    Third method added to new file

--- src/A.java
+++ src/A.java
@@ -2,11 +2,11 @@ public class A {
 
     void FirstMethod(int x) {
         for (int i = 0; i < x; i++) {
-           SecondMethod(x, i);
+            SecondMethod(x, i);
         }
     }
 
-    void SecondMethod(int x, y){
-        System.out.println(x*y-x-y);
+    void SecondMethod(int x, y) {
+        System.out.println(x * y - x - y);
     }
 }
---
+++ src/B.java
@@ -0,0 +1,9 @@
+import A;
+
+public class B {
+
+    int ThirdMethod(int a) {
+        A x = new A;
+        return SecondMethod(a, 2 * a);
+    }
+}
